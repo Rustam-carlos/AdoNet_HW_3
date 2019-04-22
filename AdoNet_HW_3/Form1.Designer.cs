50f845e9fcc34681d2059a0bb7685906fa0c7ecb
Rustam Galeev <Vincent-cat@mail.ru>
2019-04-23 05:00:24 +0600

    Добавьте файлы проекта.

---
+++ AdoNet_HW_3/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace AdoNet_HW_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.second_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id_sales = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id_client = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id_seller = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sales_summ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sales_time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(77, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выбери таблицу";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Client",
+            "Seller",
+            "sales"});
+            this.comboBox1.Location = new System.Drawing.Point(110, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(138, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(110, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Показать таблицу";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.name,
+            this.second_name});
+            this.dataGridView1.Location = new System.Drawing.Point(343, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(273, 498);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id_sales,
+            this.id_client,
+            this.id_seller,
+            this.Sales_summ,
+            this.Sales_time});
+            this.dataGridView2.Location = new System.Drawing.Point(710, 25);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(394, 498);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.Width = 30;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "name";
+            this.name.Name = "name";
+            // 
+            // second_name
+            // 
+            this.second_name.HeaderText = "second_name";
+            this.second_name.Name = "second_name";
+            // 
+            // id_sales
+            // 
+            this.id_sales.HeaderText = "id_sales";
+            this.id_sales.Name = "id_sales";
+            this.id_sales.Width = 50;
+            // 
+            // id_client
+            // 
+            this.id_client.HeaderText = "id_client";
+            this.id_client.Name = "id_client";
+            this.id_client.Width = 50;
+            // 
+            // id_seller
+            // 
+            this.id_seller.HeaderText = "id_seller";
+            this.id_seller.Name = "id_seller";
+            this.id_seller.Width = 50;
+            // 
+            // Sales_summ
+            // 
+            this.Sales_summ.HeaderText = "Sales_summ";
+            this.Sales_summ.Name = "Sales_summ";
+            // 
+            // Sales_time
+            // 
+            this.Sales_time.HeaderText = "Sales_time";
+            this.Sales_time.Name = "Sales_time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1148, 612);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn second_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_sales;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_client;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_seller;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sales_summ;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sales_time;
+    }
+}
+
---
+++ AdoNet_HW_3/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNet_HW_3
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connect = null;
+        SqlCommand comand = null;
+        SqlDataReader reader = null;
+        string connection = null;
+        string query1 = "select * from Client";
+        string query2 = "select * from Seller";
+        string query3 = "select * from sales";
+
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Load_data()
+        {
+            connection = @"Data Source = DESKTOP-OHQR73G;
+                                        Initial Catalog = Sales;
+                                        Integrated Security = True";
+            connect = new SqlConnection(connection);
+            try
+            {
+                connect.Open();
+                if (comboBox1.Text == "Client")
+                {
+                    comand = new SqlCommand(query1, connect);
+                    reader = comand.ExecuteReader();
+                    List<string[]> data = new List<string[]>();
+                    while (reader.Read())
+                    {
+                        data.Add(new string[3]);
+
+                        data[data.Count - 1][0] = reader[0].ToString();
+                        data[data.Count - 1][1] = reader[1].ToString();
+                        data[data.Count - 1][2] = reader[2].ToString();
+
+                    }
+
+                    reader.Close();
+                    connect.Close();
+
+                    foreach(string[] i in data)
+                    {
+                        dataGridView1.Rows.Add(i);
+                    }
+                }
+                else if (comboBox1.Text == "Seller")
+                {
+                    comand = new SqlCommand(query2, connect);
+                    reader = comand.ExecuteReader();
+                    List<string[]> data2 = new List<string[]>();
+                    while (reader.Read())
+                    {
+                        data2.Add(new string[3]);
+
+                        data2[data2.Count - 1][0] = reader[0].ToString();
+                        data2[data2.Count - 1][1] = reader[1].ToString();
+                        data2[data2.Count - 1][2] = reader[2].ToString();
+
+                    }
+
+                    reader.Close();
+                    connect.Close();
+
+                    foreach (string[] i in data2)
+                    {
+                        dataGridView1.Rows.Add(i);
+                    }
+                }
+                else 
+                {
+                    comand = new SqlCommand(query3, connect);
+                    reader = comand.ExecuteReader();
+                    List<string[]> data3 = new List<string[]>();
+                    while (reader.Read())
+                    {
+                        data3.Add(new string[5]);
+
+                        data3[data3.Count - 1][0] = reader[0].ToString();
+                        data3[data3.Count - 1][1] = reader[1].ToString();
+                        data3[data3.Count - 1][2] = reader[2].ToString();
+                        data3[data3.Count - 1][3] = reader[3].ToString();
+                        data3[data3.Count - 1][4] = reader[4].ToString();
+                    }
+
+                    reader.Close();
+                    connect.Close();
+
+                    foreach (string[] i in data3)
+                    {
+                        dataGridView2.Rows.Add(i);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connect?.Close();
+                comand?.Clone();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Load_data();
+        }
+
+   
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
